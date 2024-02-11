e254e33d226d0c371b8781acd87ef0a7a239ed10
yohan manohara <warushayohan80@gmail.com>
2024-02-12 02:01:07 +0530

    #

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,10 @@ import { NavLink } from 'react-router-dom'
 import {logo,menu,close} from '../assets'
 import {navLinks} from '../constant'
 
+
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className={`${
         styles.paddingX
@@ -34,13 +37,61 @@ const Navbar = () => {
           {navLinks.map((link) => (
             
 
-            <li>
+             <li 
+             key={link.id}
+             className={`${
+               active === link.title ? "text-white" : "text-secondary"
+             } hover:text-white text-[18px] font-medium cursor-pointer`}
+             onClick={() => setActive(link.title)}
+            
+
+            >
+
                   <a href={`#${link.id}`}> {link.title} </a>
             </li>
             
-          ))}
+          ))
+          }
             
           </ul>
+
+          <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            onClick={() => setToggle(!toggle)}
+          />
+          <div className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+             <ul className='list-none flex justify-end items-start flex-col gap-4'>
+          {navLinks.map((link) => (
+            
+
+             <li 
+             key={link.id}
+             className={`${
+               active === link.title ? "text-white" : "text-secondary"
+             } hover:text-white text-[18px] font-medium cursor-pointer`}
+             onClick={() => setActive(link.title)}
+            
+
+            >
+
+                  <a href={`#${link.id}`}> {link.title} </a>
+            </li>
+            
+          ))
+          }
+            
+          </ul>
+
+            
+          </div>
+            
+          </div>
 
           
         </div>
