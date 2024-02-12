2359b99a3e381da729ce2d0adc51bd8682a2ae63
yohan manohara <warushayohan80@gmail.com>
2024-02-12 22:46:30 +0530

    #

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <nav className={`${
         styles.paddingX
-      } w-full flex items-center py-1 fixed top-0 z-20  bg-primary`}>
+      } w-full flex items-center py-1 fixed top-0 z-20  bg-stone-800`}>
 
  <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
          <Link
