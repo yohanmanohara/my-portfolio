fc57fce132953c5a986bd33e662013abdb2e1de1
yohan manohara <warushayohan80@gmail.com>
2024-03-06 10:18:12 +0530

    #

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,21 +8,22 @@ const Hero = () => {
 
     <div  className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`} >
 
-         <div className='flex flex-col justify-center items-center mt-5'>
+         <div className='flex flex-col items-center justify-center mt-5'>
             <div className='w-5 h-5 rounded-full bg-stone-500'>
-            <div className='w-1 sm:h-80 h-40 violet-gradient relative top-1 left-2' />
+            <div className='relative w-1 h-40 sm:h-80 violet-gradient top-1 left-2' />
             </div>
         </div>
          <div>
            <h1 className={`${styles.heroHeadText} text-white`}> Hi, I'm <span className=' text-stone-700'>Yohan</span></h1>
            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
+            I develop 3D visuals, user <br className='hidden sm:block' />
             interfaces and web applications
           </p>
          </div>
 
     </div>
-     <ComputersCanvas/>
+    <ComputersCanvas/>
+  
   </section>
   )
 }
